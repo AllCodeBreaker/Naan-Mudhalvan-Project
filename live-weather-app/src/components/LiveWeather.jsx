efc3772bd8e876a5a2bab4dd7db3c368d561a86a
allcodebreaker <allwin2436@gmail.com>
2025-10-06 12:00:33 +0530

    Project Developed

--- live-weather-app/src/components/LiveWeather.jsx
+++ live-weather-app/src/components/LiveWeather.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import axios from "axios"
+import "./liveweather.css"
 
-const LiveWeather = ()=>{
-    const API_KEY = "31c820642b5ff306728a8ebcdfd46575"
+const LiveWeather = () => {
+    const API_KEY = import.meta.env.VITE_OPENWEATHERMAP_API;
     const [city, setCity] = useState("");
     const [weather, setWeather] = useState(null);
     const [error, setError] = useState("")
@@ -14,51 +15,54 @@ const LiveWeather = ()=>{
         "09d": "Shower Rain",
         "10d": "rain",
         "13d": "snow",
-        "50d": "mist" 
+        "50d": "mist"
 
     }
 
-    const getWeather = async ()=>{
-        if(!city) return
-        try{
+    const getWeather = async () => {
+        if (!city) return
+        try {
             const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
             console.log
-                if(res.data.cod == "200"){
-                    setWeather(res.data)
-                    setError("")
-                }
-                console.log("RESPONSE: \n", res)
-                
-        }catch(e){
+            if (res.data.cod == "200") {
+                setWeather(res.data)
+                setError("")
+            }
+
+
+        } catch (e) {
             setError("City not found!")
             setWeather(null)
         }
     }
 
-    const handleKeyPress = (e)=>{
-        if(e.key == 'Enter'){
+    const handleKeyPress = (e) => {
+        if (e.key == 'Enter') {
             getWeather()
         }
     }
 
-    return(
+    return (
         <>
-            <div>
+            <div className="weather-app-container">
                 <h1>Live Weather App</h1>
-                <input 
-                    type="text" 
-                    onChange={(e)=> setCity(e.target.value)} 
-                    value={city}
-                    onKeyPress={handleKeyPress}
-                    placeholder="Enter City" />
-                    
-                <button onClick={getWeather}>Show Details</button>
-            
-                {error && <p className="red">{error}</p>}
+                <div>
+                    <input
+                        type="text"
+                        onChange={(e) => setCity(e.target.value)}
+                        value={city}
+                        onKeyPress={handleKeyPress}
+                        placeholder="Enter City" />
+
+                    <button onClick={getWeather} >Show </button>
+
+                </div>
+
+                {error && <p className="error">{error}</p>}
 
                 {weather && (
                     <div>
-                    <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather" />
+                        {/* <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather" /> */}
                         <h2>{weather.name}, {weather.sys.country}</h2>
                         <p>Temperature: {weather.main.temp} C </p>
                         <p>Weather: {weather.weather[0].main}</p>
